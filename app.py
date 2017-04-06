b8e6ca887d103c7c07f041ed3d83c08653a37f18
Greg Stupp <gstupp@scripps.edu>
2017-04-05 17:15:26 -0700

    update for europepmc

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import sys
+sys.path.insert(0, "/home/gstupp/projects/WikidataIntegrator")
 import flask
+import traceback
 from flask import Flask, request, redirect
 from wikidataintegrator import wdi_login
 from wikidataintegrator.wdi_helpers import PubmedItem
@@ -8,38 +11,36 @@ from local import WDUSER, WDPASS
 login = wdi_login.WDLogin(user=WDUSER, pwd=WDPASS)
 app = Flask(__name__)
 
-
-def batch():
-    pmids = request.args.get('pmids', None)
-    if pmids is None:
-        print("No pmids given!")
-        return flask.jsonify({'success': False})
-    d = {}
-    for pmid in pmids.split(","):
-        p = PubmedItem(pmid)
-        d[pmid] = p.get_or_create(login)
-    return flask.jsonify({'success': True, 'result': d})
-
-
 @app.route('/')
 def home():
     return "See <a href='https://www.wikidata.org/wiki/Wikidata:WikiProject_Source_MetaData/PMIDTool'>here</a> for more info"
 
+@app.route('/get_or_create/<ext_id>')
+def get_or_create_med(ext_id=None):
+    return run(ext_id, 'MED')
+
+
+def run(ext_id, id_type='MED'):
+    try:
+        p = PubmedItem(ext_id, id_type)
+        qid = p.get_or_create(login)
+    except Exception as e:
+        traceback.print_exc()
+        return flask.jsonify({'success': False, 'message': str(e)})
+    if qid:
+        return flask.jsonify({'success': True, 'result': qid, 'errors': p.errors, 'warnings': p.warnings})
+    else:
+        return flask.jsonify({'success': False, 'errors': p.errors, 'warnings': p.warnings})
+
+
 
 @app.route('/get_or_create')
-@app.route('/get_or_create/<pmid>')
-def get_or_create(pmid=None):
-    if pmid is None:
-        return batch()
-    else:
-        p = PubmedItem(pmid)
-        wdid = p.get_or_create(login)
-        if wdid:
-            return flask.jsonify({'success': True, 'result': wdid})
-        else:
-            return flask.jsonify({'success': False})
-
-
+def get_or_create():
+    id_type = request.args.get('id_type')
+    ext_id = request.args.get('ext_id')
+    if not ext_id:
+        return flask.jsonify({'success': False, 'message': "ext_id is required"})
+    return run(ext_id, id_type)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
